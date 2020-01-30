44a39acd849098a3892ced23c55f70e33915d0d5
Christine Bumpous <bumpouce@gmail.com>
2020-01-30 02:09:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,8 @@
 require 'yaml'
 
 def load_library (library_name)
-  YAML.load_file(library_name)
+  translator = YAML.load_file(library_name)
+  pp "Returned: #{translator}"
 end
 
 def get_japanese_emoticon
