2836974fd49975a7619a631f6de7b622a2acef1c
Christine Bumpous <bumpouce@gmail.com>
2020-01-30 02:23:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,13 @@
 require 'yaml'
 
 def load_library (library_name)
+  reorganized = {:get_meaning => {}, :get_emoticon => {}}
+  
   base_data = YAML.load_file(library_name)
-  base_data.collect { |index| pp "Index: #{index}" }
+  base_data.collect do |index| 
+    pp "Index: #{index}" 
+    
+  end
   
   reorganized = {:get_meaning => {}, :get_emoticon => {}}
 end
